7f5a8251469b1f8668a98abfc9397fa0f4339ea0
Eno <43026685+EnoPM@users.noreply.github.com>
2023-11-10 19:14:47 +0100

    Update broadcast version

--- BetterOtherRoles/Patches/ConstantsPatch.cs
+++ BetterOtherRoles/Patches/ConstantsPatch.cs
@@ -5,7 +5,7 @@ namespace BetterOtherRoles.Patches {
     public static class ConstantsPatch {
         public static void Postfix(ref int __result) {
             if (AmongUsClient.Instance.NetworkMode == NetworkModes.OnlineGame)
-                __result = Constants.GetVersion(2222, 0, 0, 0);
+                __result += 25;
         }
     }
 }
